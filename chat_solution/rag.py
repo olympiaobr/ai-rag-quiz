0413e54c2a58bb41e0984a06486463ea06deaa7f
JeanMachado <jean.machado@getyourguide.com>
2024-11-18 23:15:07 +0100

    improve prompt quality

--- chat_solution/rag.py
+++ chat_solution/rag.py
@@ -9,54 +9,55 @@ class LearningAssistant:
         self.llm = llm
         self.conversation_history = []
 
-    def _create_question_prompt(self, context: str, query: str) -> str:
+    def _create_question_prompt(self, documents: str, query: str) -> str:
 
-        chat_history = "\n".join([f"user: {query}\nassistant: {response}" for query, response in self.conversation_history])
+        chat_history = ""
+        i = 1
+        for query, response in self.conversation_history:
+            chat_history += f"Interaction {i}\nUser: {query}\nAssistant: {response}\n"
+            i += 1
 
         return f""" You are a helpful AI knowledge quiz chat assistant. Your goal is to test the knowledge of the users on the given topic."
-When the user asks for a topic (the topic could also come in the form of a question), you should:
-- Generate a new relevant question based on the context and the topic provided. The topic you select on the context does not need to be exactly the same, but should be related to the topic.
-The question should be relevant to the given topic and the answer should be found within the given context. In case the selected topic is not found in the context, say that the topic is not found and give a list of possible topics consisting of 2-3 most relevant examples. You are only to generate questions based on topics which are found in the context. If the topic is found within the context, only generate questions for which the answer can also be found within the context.
+The user gives you a topic or a question and you should generate a new relevant quiz based on the context.
+- Generate a new relevant quiz question based on the context and the topic provided. The topic you select on the context does not need to be exactly the same, but should be related to the topic.
+- The question should be relevant to the given topic and the answer should be found within the given context. If not say: "You did not equip me with the knowledge to answer this question."
 - Provide 4 answer choices for the question, one of which should be correct and the other three should be incorrect but plausible. Answer choices should be formulated clearly and concisely.
 - Mark the index of the correct answer in the answer choices list with the pattern (CORRECT) in the end
 - Provide an explanation for the correct answer after the user selected an answer. The explanation should give the user additional context and help them better understand the topic.
 if the user answers with a number, it is because they selected an answer to the previous question. In this case, you should evaluate if the answer is correct or not and provide feedback to the user.
+- Do not mention the context in your response.
 
-<newquestion>
-Chat history: 
-New Context: the context provided to you
+Example below:
+Interaction 1
+New Context: LLMs are large language models that can generate responses to user queries. They are trained on massive datasets to learn patterns, structures, and relationships in text. They can generate responses by combining language generation with real-time data retrieval.
 User input: How do LLMs generate responses?
-Assistant:<startanswer>
+Assistant:
+Question: How do LLMs generate responses?
 1. LLMs generate responses by searching the internet for relevant information. 
 2. LLMs generate responses by learning patterns, structures, and relationships in text from massive datasets. (CORRECT)
 3. LLMs generate responses by combining language generation with real-time data retrieval.
-4. LMs generate responses by using a predefined set of rules and templates.<endanswer>
-<endquestion>
-<newquestion>
-Chat history:<starthistory>
-user: How do LLMs generate responses?
-assistant: 1. LLMs generate responses by searching the internet for relevant information. 
-2. LLMs generate responses by learning patterns, structures, and relationships in text from massive datasets. (CORRECT)
-3. LLMs generate responses by combining language generation with real-time data retrieval.
-4. LMs generate responses by using a predefined set of rules and templates.<endhistory>
-New Context: the context provided to you
-User input: 2.
-Assistant:<startanswer>Correct! LLMs generate responses by learning patterns, structures, and relationships in text from massive datasets.
-<endanswer>
-<endquestion>
+4. LMs generate responses by using a predefined set of rules and templates.
+Interaction 2
+User input: 3
+Assistant:Incorrect! There is no need of realtime data retrieval.
+Interaction 3
+User input: 2
+Assistant:Correct! LLMs generate responses by learning patterns, structures, and relationships in text from massive datasets.
 
-Just continue with 1 question at a time. Be direct and concise.
-Chat history: {chat_history}
-New Context: {context}
+Now we start the conversation history.
+{chat_history}
+
+Just with the next answer
+New Context: {documents}
+Interaction {i+1}
 User input: {query}
-Assistant:<startanswer>"""
+Assistant:"""
 
     def call_llm(self, query: str) -> dict:
         documents = self.embedding_db.retrieve(query)
-
-        context = "\n".join(documents)
+        print(documents)
         # only append the last 1 messages to the context
-        prompt = self._create_question_prompt(context, query)
+        prompt = self._create_question_prompt(documents, query)
         response = self.llm.call(prompt)
         self.conversation_history.append((query, response))
 
