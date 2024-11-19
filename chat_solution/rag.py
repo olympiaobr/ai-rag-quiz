268fa31ffc808488135c35af007a6afd1083c009
JeanMachado <jean.machado@getyourguide.com>
2024-11-19 22:39:25 +0100

    chat working

--- chat_solution/rag.py
+++ chat_solution/rag.py
@@ -1,36 +1,46 @@
 import json
 
 from chat_solution.embedding_db import EmbeddingDatabase
+from chat_solution.llm import LargeLanguageModel
 
 class LearningAssistant:
-    def __init__(self, embedding_db, llm):
-        self.embedding_db : EmbeddingDatabase = embedding_db
+    _instance = None
 
-        self.llm = llm
+    def get_instance():
+        if not LearningAssistant._instance:
+            LearningAssistant._instance = LearningAssistant()
+        return LearningAssistant._instance
+    
+    def __init__(self):
+        self.embedding_db = EmbeddingDatabase()  # Remove the embedding_model argument
+        self.llm = LargeLanguageModel()
         self.conversation_history = []
+        print("Quis assistant initialized")
 
     def _create_question_prompt(self, documents: str, query: str) -> str:
 
         chat_history = ""
         i = 1
-        for query, response in self.conversation_history:
-            chat_history += f"Interaction {i}\nUser: {query}\nAssistant: {response}\n"
+        print(self.conversation_history)
+        for old_query, response in self.conversation_history:
+            chat_history += f"Interaction {i}\nUser: {old_query}\nAssistant: {response}\n"
             i += 1
         
-        new_context_str = f"New Context: {documents}" if documents else ""
+        new_context_str = f"\nNew Context: {documents}" if documents else ""
 
         return f""" You are a helpful AI knowledge quiz chat assistant. Your goal is to test the knowledge of the users on the given topic."
 The user gives you a topic or a question and you should generate a new relevant quiz based on the context.
 - Generate a new relevant quiz question based on the context and the topic provided. The topic you select on the context does not need to be exactly the same, but should be related to the topic.
+- Your primary audience are students learning about AI. Do not use technical jargon that is not common knowledge or that you dont explain first.
 - The question should be relevant to the given topic and the answer should be found within the given context. If not say: "You did not equip me with the knowledge to answer this question."
 - Provide 4 answer choices for the question, one of which should be correct and the other three should be incorrect but plausible. Answer choices should be formulated clearly and concisely.
 - Mark the index of the correct answer in the answer choices list with the pattern (CORRECT) in the end
 if the user answers with a number, it is because they selected an answer to the previous question. In this case, you should evaluate if the answer is correct or not and provide feedback to the user.
 - Do not mention the context in your response.
-- Provide an explanation for the correct answer after the user selected an answer. The explanation should give the user additional context and help them better understand the topic.
+- Provide an explanation for previous question after the user selected an answer. The explanation should give the user additional context and help them better understand the topic.
 - Do not generate questions that are not about AI or ML.
 
-Example below:
+<startexample>
 Interaction 1
 New Context: LLMs are large language models that can generate responses to user queries. They are trained on massive datasets to learn patterns, structures, and relationships in text. They can generate responses by combining language generation with real-time data retrieval.
 User input: How do LLMs generate responses?
@@ -42,13 +52,17 @@ Question: How do LLMs generate responses?
 4. LMs generate responses by using a predefined set of rules and templates.
 Interaction 2
 User input: 3
-Assistant:Incorrect! LLMs generate responses by learning patterns, structures, and relationships in text from massive datasets. Try another topic!
+Assistant:Incorrect! LLMs generate responses by learning patterns, structures, and relationships in text from massive datasets.
+Interaction 3
+User input: 2
+Assistant:Correct! LLMs generate responses by learning patterns, structures, and relationships in text from massive datasets.
+</endexample>
 
-Now we start the conversation history.
+Now we start the conversation history:
 {chat_history}
 
-Interaction {i+1}
-{new_context_str}
+Just predict the next answer:
+Interaction {i+1} {new_context_str}
 User input: {query}
 Assistant:"""
 
@@ -57,10 +71,9 @@ Assistant:"""
         # do not populate the context if the user input is a number as we are answering a previous question
         # answer can come as a string number like "2", treat it as a number
         if not query.isnumeric():
-            print(f"Query is not numeric so we will use the context")
             documents = self.embedding_db.retrieve(query)
-            print(documents)
         prompt = self._create_question_prompt(documents, query)
+        print(prompt)
         response = self.llm.call(prompt)
         self.conversation_history.append((query, response))
 
--- chat_solution/start_streamlit.py
+++ chat_solution/start_streamlit.py
@@ -1,15 +1,9 @@
 import streamlit as st
 
-from embedding_db import EmbeddingDatabase
-from embedding_model import EmbeddingModel
-from llm import LargeLanguageModel
 from rag import LearningAssistant
 
 # Initialize models and RAG
-embedding_model = EmbeddingModel()
-embedding_db = EmbeddingDatabase()  # Remove the embedding_model argument
-llm = LargeLanguageModel()
-rag = LearningAssistant(embedding_db, llm)
+rag = LearningAssistant.get_instance()
 
 # Streamlit app title
 st.title("LLM Workshop Quiz")
